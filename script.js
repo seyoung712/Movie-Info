1c9e87b1f97e0ff9754cf92f10b8b1e9ec570c73
ParkSeyoung <hsj0069@naver.com>
2024-04-10 11:48:00 +0900

    2: 비동기 처리

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//?sort_by=like_count
+const getMovie = async() => {
+    const url = new URL(`https://yts.mx/api/v2/list_movies.json`);
 
-const getMovie = () => {
-    const url = new URL(`https://yts.mx/api/v2/list_movies.json?sort_by=like_count`);
-    console.log("uuu",url);
+    const response = await fetch(url);
+    const result = await response.json();
+    let movie = result.data.movies;
+    console.log("data",movie);
 }
 
 getMovie();
