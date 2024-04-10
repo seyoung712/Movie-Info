538405f969a8cba4804c74da1e700632631a60ca
ParkSeyoung <hsj0069@naver.com>
2024-04-10 12:27:47 +0900

    3: html & css

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //?sort_by=like_count
+
+let movie=[]
+
 const getMovie = async() => {
     const url = new URL(`https://yts.mx/api/v2/list_movies.json`);
 
     const response = await fetch(url);
     const result = await response.json();
-    let movie = result.data.movies;
+    movie = result.data.movies;
     console.log("data",movie);
 }
 
