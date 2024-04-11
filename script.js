772b42b96d8887059561ffe574e6c95128d2e5a1
ParkSeyoung <hsj0069@naver.com>
2024-04-12 00:58:02 +0900

    6: category -ing

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-//?sort_by=like_count
+let movieListTop = [];
+let movieListAll = [];
 
-let movieList = [];
 const category = document.querySelectorAll(".category button");
-console.log("cc",category);
+category.forEach((category) =>
+  category.addEventListener("click", (event) => getMovieCategory(event))
+);
 
 const getMovie = async () => {
-  const url = new URL(`https://yts.mx/api/v2/list_movies.json?sort_by=like_count`);
+  const url = new URL(
+    `https://yts.mx/api/v2/list_movies.json?sort_by=like_count`
+  );
 
   const response = await fetch(url);
   const result = await response.json();
-  movieList = result.data.movies;
+  movieListTop = result.data.movies;
+  render();
+  console.log("data", movieListTop);
+};
+
+/*genre=${category}*/
+const getMovieCategory = async(event) => {
+  const category = event.target.textContent
+  console.log("category",category);
+  const url = new URL(`https://yts.mx/api/v2/list_movies.json`);
+
+  const response = await fetch(url);
+  const result = await response.json();
+  movieListAll = result.result.movies;
   renderAll();
-  console.log("data", movieList);
+  console.log("data",movieListAll);
 };
 
-/*
-const categoryEngToKor{
-    if(Action){
-        return "액션"
-    }
-};
-*/
 
-/*
-document.querySelector('.slide-btn').addEventListener('click', () => {
-    document.querySelector('.container-slide').style.transform = 'translate(-100%)';
-}); 
-*/
-//https://www.youtube.com/watch?v=qHzSQrLjxlQ
-//java 가로 슬라이드
-
-const renderAll = () => {
-  const movieHTML = movieList
-    .map((movie) =>
+/* TOP20 */
+const render = () => {
+  const movieHTML = movieListTop
+    .map(
+      (movie) =>
         `<span class="col-lg-2 card movie-inner">
             <div>
                 <div>
@@ -49,7 +53,32 @@ const renderAll = () => {
     )
     .join("");
 
-  document.getElementById("movie-list").innerHTML = movieHTML;
+  document.getElementById("movie-list-top").innerHTML = movieHTML;
 };
 
+/*전체*/
+const renderAll = () => {
+    const movieAllHTML = movieListAll
+      .map(
+        (movie) =>
+          `<span class="col-lg-2 card movie-inner">
+              <div>
+                  <div>
+                  <img class="movie-img-size card-body" src="${movie.medium_cover_image}"/>
+              </div>
+              <div class="movie-content">
+                  <div class="movie-title card-title"><h5>${movie.title}</h5></div>
+                  <br>
+                  <div class="movie-genres card-text">${movie.genres}</div>
+                  <div class="movie-etc card-text">Rating ${movie.rating} / Runtime ${movie.runtime}</div>
+              </div>
+              </div>
+          </span>`
+      )
+      .join("");
+  
+    document.getElementById("movie-list-all").innerHTML = movieAllHTML;
+  };
+
 getMovie();
+getMovieCategory();
